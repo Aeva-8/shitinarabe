8962e028148dbf974e7faacba9da55adfa6f50df
Aeva-8 <urimaru9@gmail.com>
2021-02-18 20:04:17 +0900

    Check7card

--- Assets/Scripts/DropPlace.cs
+++ Assets/Scripts/DropPlace.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -12,7 +14,44 @@ public class DropPlace : MonoBehaviour, IDropHandler
         Card_movement card = eventData.pointerDrag.GetComponent<Card_movement>(); // ドラッグしてきた情報からCardMovementを取得
         if (card != null) // もしカードがあれば、
         {
-            card.Field = this.transform; // カードの親要素を自分（アタッチされてるオブジェクト）にする
+            int card_num = int.Parse(card.name);
+            if (Check(card_num))
+            {
+                if (card_num % 13 > 6)
+                {
+                    //7以上
+                    GameObject chil_tmp = this.transform.Find("after_7").gameObject;
+                    card.Field = chil_tmp.transform;
+                }
+                else
+                {
+                    GameObject chil_tmp = this.transform.Find("before_7").gameObject;
+                    card.Field = chil_tmp.transform;
+                }
+                //Playerhandからカードデータを消去し、Fieldにカードデータを追加する
+                //Turn_stateを0にする
+                GameData.PlayerHand[GameData.Turn].Remove(card_num);
+                GameData.Field[int.Parse(this.name)].Add(card_num);
+                main.Turn_State = 0;
+
+            }
+
+
         }
+    }
+    bool Check(int card_num)
+    {
+        int sort = int.Parse(this.name);
+        if ( sort== card_num / 13)
+        {
+            Debug.Log("Field : "+GameData.Field.Count());
+            //ソートが正しいか評価
+            if (card_num == GameData.Field[sort].Min() - 1 || card_num == GameData.Field[sort].Max() + 1)
+            {
+                //置ける数字か評価
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -2,13 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class GameData
 {
+
     public static int RoomId;
     public static int GameStatus;
     public static int PlayerLimit=4;
+    public static int Turn = 0;
+    //Turnはどのプレイヤーのターンかの情報を持つ
     public static List<List<int>> PlayerHand =new List<List<int>>();
     public static List<List<int>> Field = new List<List<int>>();
 
@@ -33,9 +37,21 @@ public class GameData
 
 public class main : MonoBehaviour
 {
+    public static int Turn_State = -1;
+    //Turn_State 0はターンエンド状態,1はプレイできる状態-1でほかの人のターンまたその他のターン
+    [SerializeField] GameObject cardPrefab;
+    [SerializeField] Transform playerField;
     List<int> Shuffle(List<int> list)
     {
         return list = list.OrderBy(i => Guid.NewGuid()).ToList();
+    }
+    void Generate_Field()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            List<int> add_tmp = new List<int>();
+        }
+        
     }
     void Generate_Card()
     {
@@ -61,16 +77,63 @@ public class main : MonoBehaviour
             }
             
         }
+        
     }
-
+    void Check7()
+    {
+        int tmp;
+        for (int i = 0; i < 4; i++)
+        {
+            //ここから7を抜く
+            for (int j = 0; j < GameData.PlayerLimit; j++)
+            {
+                tmp = 6 + 13 * i;
+                if (GameData.PlayerHand[j].Contains(tmp))
+                {
+                    GameData.PlayerHand[j].Remove(tmp);
+                    GameData.Field[tmp / 13].Add(tmp);
+                }
+            }
+        }
+       
+    }
     void Generate_Hand()
     {
+        foreach (int i in GameData.PlayerHand[0])
+        {
+            GameObject obj= Instantiate(cardPrefab, playerField);
+            obj.name = i.ToString();
+            TextMeshProUGUI tm = obj.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+            string tmp="";
+            switch (i / 13)
+            {
+                case 0:
+                    tmp = "♠";
+                    break;
+                case 1:
+                    tmp = "♥";
+                    break;
+
+                case 2:
+                    tmp = "♣";
+                    break;
+
+                case 3:
+                    tmp = "♦";
+                    break;
+            }
+            Debug.Log(tm.text);
+            tm.text = tm.text.Replace("x", ((i%13)+1).ToString());
+            tm.text = tm.text.Replace("d", tmp);
+        }
 
     }
     // Start is called before the first frame update
     void Start()
     {
+        Generate_Field();
         Generate_Card();
+        Generate_Hand();
         Debug.Log(GameData.GetPlayerLimit());
         for (int i = 0; i < GameData.GetPlayerLimit(); i++)
         {
